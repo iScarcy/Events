b5878870a63821342d605bfcf84fc04ed77ca5db
iScarcy <scarcella.giuseppe@gmail.com>
2024-03-20 23:29:37 +0100

    ho aggiunto un componente sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/app/shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    SidenavComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
