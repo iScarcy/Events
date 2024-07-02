191f7906502b9c7dcb3c53eaa00c556881e5555c
iScarcy <56608800+iScarcy@users.noreply.github.com>
2024-07-02 18:04:52 +0200

    ho aggiunto un nuovo servizio per carcare gli eventi tramite le date

---
+++ src/app/models/interfaces/IDaysEvents.ts
@@ -0,0 +1,4 @@
+export interface IDaysEvents {
+    from:Date,
+    to:Date
+}
--- src/app/services/recurring-events.service.ts
+++ src/app/services/recurring-events.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { IEvents } from '../models/interfaces/IEvents';
 import { baseApiUrl } from '../app.constant';
+import { IDaysEvents } from '../models/interfaces/IDaysEvents';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,18 @@ export class RecurringEventsService {
 
   getEvents(eventsType:string):Observable<IEvents[]>{
     return this.httpEvents.get<Array<IEvents>>(baseApiUrl+eventsType).pipe(
+      map(events => events.map(event => ({ type:event.type, date: event.date, description: event.description})))
+    );
+  }
+
+  getEventsByDays(request:IDaysEvents ):Observable<IEvents[]>{
+    
+    const body = {
+      from: request.from,
+      to: request.to
+    }
+    
+   return this.httpEvents.put<Array<IEvents>>(baseApiUrl+"days", body).pipe(
       map(events => events.map(event => ({ type:event.type, date: event.date, description: event.description})))
     );
   }
